65f3555b717b89b136be23b286a2dea68d13fed5
Rémi <remi.larrouquis@gmail.com>
2026-03-14 22:45:00 +0100

    chore: [nx migration] update-module-resolution-22-2-0

--- apps/nx-exploration/src/app/app.config.ts
+++ apps/nx-exploration/src/app/app.config.ts
@@ -17,6 +17,7 @@ export const appConfig: ApplicationConfig = {
       theme: {
         preset: Aura,
         options: {
+          darkModeSelector: "none",
           cssLayer: {
             name: 'primeng',
             order: 'theme, base, primeng',
